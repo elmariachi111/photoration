a036daaa53cef1437e54f4adc5598cae02a34b42
Stefan <stadolf@gmail.com>
2013-06-02 09:12:59 +0200

    get browser location

--- public/javascripts/photoration.js
+++ public/javascripts/photoration.js
@@ -5,10 +5,20 @@ PHR.Router = Backbone.Router.extend({
 });
 
 PHR.PageMain = Backbone.View.extend({
+    events: {
+        "click #btn-curLoc":"getPos"
+    },
     initialize: function() {
+        this.DEFAULT_LOCATION = {
+            coords: {
+                latitude: 52.516012,
+                longitude: 13.418126
+            }
+        }
+        var defaultLocLL = new google.maps.LatLng(this.DEFAULT_LOCATION.coords.latitude, this.DEFAULT_LOCATION.coords.longitude);
         var mapOptions = {
-            center: new google.maps.LatLng(-34.397, 150.644),
-            zoom: 8,
+            center: defaultLocLL,
+            zoom: 10,
             mapTypeControl: false,
             zoomControlOptions: {
                 style: google.maps.ZoomControlStyle.SMALL
@@ -16,8 +26,42 @@ PHR.PageMain = Backbone.View.extend({
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         this.gmap = new google.maps.Map(document.getElementById('gmap'),  mapOptions);
-    },
+        google.maps.event.addListener(self.gmap, 'click', this.onMapClicked.bind(this));
 
+        this.curMarker = new google.maps.Marker({
+            position: defaultLocLL,
+            map: this.gmap,
+            animation: google.maps.Animation.DROP
+        });
+    },
+    getPos: function() {
+        var self = this;
+        this.getNavigatorLocation( function(pos) {
+            var gmPos = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+            self.gmap.setCenter(gmPos);
+            self.gmap.setZoom(15);
+
+            self.curMarker.setPosition(gmPos);
+        })
+
+    },
+    getNavigatorLocation: function(callback) {
+        var self = this;
+        navigator.geolocation.getCurrentPosition(
+            function(coords) {
+                if (typeof coords == "undefined")
+                    return callback(self.DEFAULT_LOCATION);
+                else
+                    return callback(coords);
+            },
+            function() { console.log("error"); callback(self.DEFAULT_LOCATION); },
+            {'enableHighAccuracy':true,'timeout':10000,'maximumAge':0}
+        );
+        return true;
+    },
+    onMapClicked: function() {
+
+    },
     show: function() {
         $('.page').addClass('hide');
         this.$el.removeClass('hide');
