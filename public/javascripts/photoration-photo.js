d48fa32eb9075c91c827c06cfab8625b5d7bed43
Stefan <stadolf@gmail.com>
2013-06-02 12:59:56 +0200

    2nd map

--- public/javascripts/photoration-photo.js
+++ public/javascripts/photoration-photo.js
@@ -1,7 +1,7 @@
 PHR.PagePhoto = Backbone.View.extend({
 
     initialize: function() {
-        this.$img = this.$('#theImage');
+        this.$imgDiv = this.$('#theImage');
     },
     show: function(eyeemid) {
         $('.page').addClass('hide');
@@ -10,9 +10,38 @@ PHR.PagePhoto = Backbone.View.extend({
         var self = this;
 
         $.get("/getPhotoDetails", {id:eyeemid}, function(resp) {
-            console.dir(resp);
             var image = new PHR.Photo(resp.photo);
-            self.$img.attr("src", image.get('photoUrl'));
+            var dt = new Date(resp.photo.updated);
+
+            var $img = $('<img src="'+image.get('photoUrl')+'">');
+            self.$imgDiv.html($img);
+
+            $('#time').html(dt.toLocaleTimeString());
+            $('#date').html(dt.toLocaleDateString());
+            $('#filter').html(resp.photo.filter);
+            $('#likes').html(resp.photo.likers.total);
+
+            var ll = new google.maps.LatLng(resp.photo.latitude, resp.photo.longitude);
+            var gmap = self.showMap(ll);
+
         });
+    },
+    showMap: function(ll) {
+        var mapOptions = {
+            center: ll,
+            zoom: 15,
+            mapTypeControl: false,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var gmap = new google.maps.Map(document.getElementById('gmap2'),  mapOptions);
+        this.curMarker = new google.maps.Marker({
+            position: ll,
+            map:  gmap,
+            animation: google.maps.Animation.DROP
+        });
+        return gmap;
     }
 });
